490e57ddb68fa60c8c629bdbe83c421cf2c669dd
trevor <trevor@rimmer.local>
2021-10-24 00:56:50 -0400

    nobody uses zoom

--- zoom.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# download, install, and clean up Zoom
-wget https://zoom.us/client/latest/zoom_amd64.deb -P /home/trevor/Downloads
-sudo dpkg -i /home/trevor/Downloads/zoom_amd64.deb
-for i in $(ls /home/trevor/Downloads/*zoom*); do rm -f $i; done
-exit
